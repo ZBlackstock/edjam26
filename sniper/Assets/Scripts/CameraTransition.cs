24dbc7073b5bbc996e74e0f18b2139a37b2a5f75
ZBlackstock <40592313@live.napier.ac.uk>
2026-01-31 15:52:42 +0000

    works

--- sniper/Assets/Scripts/CameraTransition.cs
+++ sniper/Assets/Scripts/CameraTransition.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CameraTransition : MonoBehaviour
 {
     InputManager input;
-    [SerializeField] private Transform[] TPs;
+    private Transform currentTP;
+    [SerializeField] private Transform[] TPs; // 0 - building, 1 - street, 2 - room
+    [SerializeField] private SpriteRenderer sniper;
+    [SerializeField] private SpriteRenderer handgun;
+    [SerializeField] private GameObject postProcessing;
 
     private void Awake()
     {
@@ -12,22 +18,44 @@ public class CameraTransition : MonoBehaviour
 
     void Start()
     {
-        
+        currentTP = TPs[0]; // Set to aim sniper at building on start
     }
 
     void Update()
     {
-        if(input.moveLeftAction.WasPressedThisFrame() || input.moveRightAction.WasCompletedThisFrame())
-        {
-            print("Room");
-        }
-        else if (input.moveUpAction.WasPressedThisFrame())
+        if (input.moveUpAction.WasPressedThisFrame())
         {
             print("Building");
+            if (currentTP != TPs[0])
+            {
+                SetAim(TPs[0], true);
+            }
         }
         else if (input.moveDownAction.WasPressedThisFrame())
         {
             print("Street");
+            if (currentTP != TPs[1])
+            {
+                SetAim(TPs[1], true);
+            }
         }
+        else if (input.moveLeftAction.WasPressedThisFrame() || input.moveRightAction.WasCompletedThisFrame())
+        {
+            print("Room");
+            if (currentTP != TPs[2])
+            {
+                SetAim(TPs[2], false);
+            }
+        }
+    }
+
+    private void SetAim(Transform tp, bool useSniper)
+    {
+        currentTP = tp;
+        transform.position = currentTP.position;
+
+        sniper.enabled = useSniper;
+        handgun.enabled = !useSniper;
+        postProcessing.SetActive(useSniper);
     }
 }
