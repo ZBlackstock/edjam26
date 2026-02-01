6eea2c1bc66bbeb9077da32085ab00979a245f0c
Kaelin-Walford <40650381@live.napier.ac.uk>
2026-02-01 16:04:36 +0000

    Enemies Kill You

--- sniper/Assets/Scripts/Bullet.cs
+++ sniper/Assets/Scripts/Bullet.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    SoundManager sound;
     void Start()
     {
         Destroy(gameObject, 0.1f);
+        sound = FindFirstObjectByType<SoundManager>();
+        sound.StopSound(sound.death);
     }
 }
