b9580a3c448796ee93b330a3664f955942ea1f1a
ZBlackstock <40592313@live.napier.ac.uk>
2026-02-01 15:56:58 +0000

    Spawning enemies

--- sniper/Assets/Scripts/TopDown_EnemySpawner.cs
+++ sniper/Assets/Scripts/TopDown_EnemySpawner.cs
@@ -3,22 +3,23 @@ using UnityEngine;
 public class TopDown_EnemySpawner : MonoBehaviour
 {
     public bool spawn;
-    public GameObject enemy;
+    public int number;
+    public GameObject[] enemies;
 
     private void Update()
     {
         if (spawn)
         {
-            SpawnEnemy();
+            SpawnEnemy(number);
             spawn = false;
         }
     }
 
-    public void SpawnEnemy()
+    public void SpawnEnemy(int num)
     {
        float x = Random.Range(40f, 70);
        float y = Random.Range(4f, 10);
 
-        Instantiate(enemy, new Vector2(x, y), Quaternion.identity);
+        Instantiate(enemies[num], new Vector2(x, y), Quaternion.identity);
     }
 }
