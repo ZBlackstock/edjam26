f33dde1405a7166b94247e266f13150f9caf03d1
ZBlackstock <40592313@live.napier.ac.uk>
2026-02-01 13:11:35 +0000

    NPCs move smoothly

--- sniper/Assets/Scripts/NPC_TopDown.cs
+++ sniper/Assets/Scripts/NPC_TopDown.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Threading;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -13,28 +14,40 @@ public class NPC_TopDown : MonoBehaviour
         StartCoroutine(NPC_Move());
     }
 
-    float targetZ = 0;
-    float startZ = 0;
     float duration = 0;
     float timer = 0;
-
+    private Vector3 targetPos;
     float x, y;
+    Vector3 startPos;
 
     private IEnumerator NPC_Move()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(1, 3));
+            timer = 0;
+            while(timer < Random.Range(2f, 5f))
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
 
             x = Random.Range(40f, 70);
             y = Random.Range(-9f, 10);
+            targetPos = new Vector3(x, y, 0);
+
+            startPos = transform.position;
 
             timer = 0;
             duration = Random.Range(1f, 5);
 
-            while (timer < duration)
+            while (transform.position != targetPos)
             {
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3(x, y, 0), moveSpeed * Time.deltaTime);
+                transform.position = new Vector3(Mathf.Lerp(startPos.x, targetPos.x, timer / duration),
+                    Mathf.Lerp(startPos.y, targetPos.y, timer / duration),0);
+
+                Vector2 direction = targetPos - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+                transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
                 timer += Time.deltaTime;
                 yield return null;
